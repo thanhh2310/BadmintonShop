6558440729469b8370e91afcc5c5a1161ab5721d
thanh2310 <tienthanhv37@gmail.com>
2025-10-13 21:51:06 +0700

    start project

---
+++ BadmintonShop/BadmintonShop/src/main/java/com/example/BadmintonShop/BadmintonShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.BadmintonShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BadmintonShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BadmintonShopApplication.class, args);
+	}
+
+}
